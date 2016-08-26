a564a962dcc3805501dc0a2245990763f25662bd
Zhu Yong <zhuyong.me@gmail.com>
2016-08-26 17:13:48 +0800

    add simple callback and handler

--- handler.go
+++ handler.go
@@ -5,6 +5,7 @@ import (
 )
 
 type Callback interface {
+	Success()
 	Failure()
 	Done() bool
 }
@@ -19,10 +20,40 @@ type SimpleCallback struct {
 	done bool
 }
 
+func (c *SimpleCallback) Success() {
+	c.done = true
+}
+
 func (c *SimpleCallback) Failure() {
 	c.done = true
 }
 
 func (c *SimpleCallback) Done() bool {
 	return c.done
+}
+
+type SimpleHandler struct {
+	callback *Callback
+}
+
+func (h *SimpleHandler) Handle(cmd *kproto.Command, value []byte) error {
+	if h.callback != nil {
+		if cmd.Status != nil && cmd.Status.Code != nil {
+			if cmd.GetStatus().GetCode() == kproto.Command_Status_SUCCESS {
+				(*h.callback).Success()
+			} else {
+				(*h.callback).Failure()
+			}
+		}
+
+	}
+	return nil
+}
+
+func (h *SimpleHandler) Error() {
+
+}
+
+func (h *SimpleHandler) SetCallback(call *Callback) {
+	h.callback = call
 }
